9fa4c51561b7ec492eb3e2ffd66b1a847b098673
Diego Vaccher <me@dvaccher.com>
2025-03-23 14:18:36 +0100

    upd: more exceptions

---
+++ scv2_api/exception.py
@@ -0,0 +1,8 @@
+
+class SCV2InvalidEndpointException(Exception): pass
+
+class SCV2ConnectionError(Exception): pass
+
+class SCV2TimeoutError(Exception): pass
+
+class SCV2RequestError(Exception): pass
